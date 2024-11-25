bd943ed6473deac40e4df7a758db024789ef393f
SidAlex <palach.orl@mail.ru>
2024-11-26 00:28:37 +0300

    add command to exec

--- cmd/stateFileCHecker/main.go
+++ cmd/stateFileCHecker/main.go
@@ -5,13 +5,17 @@ import (
 	"github.com/SidorkinAlex/stateFileChecker/internal/CliTextColor"
 	"github.com/SidorkinAlex/stateFileChecker/internal/CourceAnalyser"
 	"log"
+	"os/exec"
 )
 
 func main() {
 	Args := CliApgParser.GetArgs()
 	successCHeck, fileDiff := CourceAnalyser.CheckHashes(Args)
 	if successCHeck {
+		exec.Command("/bin/sh", "-c", Args.SuccessCommand).Output()
 		log.Println(CliTextColor.SetGreenColor("the consistency of the directory has been successfully checked " + Args.Sources))
 	}
+	exec.Command("/bin/sh", "-c", Args.FailedCommand).Output()
 	log.Fatal("\n" + fileDiff)
+
 }
